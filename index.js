ddabb9a73f28d258d43045de0fe077101fbb7225
vlorian <vlorian.dervishi@gmail.com>
2023-07-02 11:13:06 -0400

    added main with id main using dom

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Write your code here!
-const element=document.createElement("div");
+const element = document.createElement("div");
 
 document.body.append(element);
 
-const ul=document.createElement("ul");
+const ul = document.createElement("ul");
 
-for(let i=0;i<3;i++){
-    const li=document.createElement("li");
-    li.textContent=(i+1).toString();
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
     ul.append(li);
     element.append(ul);
+    li.textContent = "Hi There!"
+    console.log(li);
 }
+const main = document.getElementById("main");
+main.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
