44a9884b0a43b8a708a5863607f38651db561d8c
vlorian <vlorian.dervishi@gmail.com>
2023-07-02 14:35:14 -0400

    assignement completed

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // Write your code here!
-const element = document.createElement("div");
 
-document.body.append(element);
+const main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = "new header created here";
 
-const ul = document.createElement("ul");
+newHeader.id = "victory";
 
-for (let i = 0; i < 3; i++) {
-    const li = document.createElement("li");
-    li.textContent = (i + 1).toString();
-    ul.append(li);
-    element.append(ul);
-    li.textContent = "Hi There!"
-    console.log(li);
-}
-const main = document.getElementById("main");
-main.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+newHeader.innerHTML = "Vlorian Dervishi is the champion";
+
+// main.remove();
+document.body.append(newHeader);
